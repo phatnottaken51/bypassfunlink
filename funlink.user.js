7d44ef9c74e5c2bce8c5cc1e5687e41a09125469
phatnottaken51 <vanphat51nbs2010@gmail.com>
2025-07-13 15:07:16 +0700

    Create funlink.user.js

---
+++ funlink.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Funlink.io Bypass
+// @namespace    https://github.com/phatnottaken51/
+// @version      3.1.0
+// @description  Tự động bypass funlink.io với nhiều loại nhiệm vụ khác nhau
+// @require      https://raw.githubusercontent.com/phatnottaken51/phatnottaken/refs/heads/main/funlink.js
+// @author       PhatNotTaken
+// @match        https://funlink.io/*
+// @grant        GM_xmlhttpRequest
+// @grant        GM_addStyle
+// @run-at       document-start
+// @icon         https://cdn.discordapp.com/avatars/1001691686721835090/27fe01cea4a0e39c19051ed609ced02b.png
+// ==/UserScript==
